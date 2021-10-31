e0e001d7c589e7b0081b57a02ae615cec3394bd0
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-11-01 04:24:56 +0530

    added interface

--- index.ts
+++ index.ts
@@ -1,7 +1,21 @@
 import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
-
+//interface
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+the do with: ${id}
+has title of ${title}
+finshed ? ${completed}
+`);
 });
